5ea335301d5bdc9f2c85d73e9a3b749f7f8a45bc
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-01-28 11:31:13 -0300

    setting the typescript in the project

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-criar o next app:
+### criar o next app:
 npx create-next-app nextjs-blog --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
 
-rodar o projeto:
+### rodar o projeto:
 npm run dev
 http://localhost:3000
+
+### instalar o typescript
+- criar o tsconfig.json
+- rodar o comando npm install --save-dev typescript @types/react @types/node
+- rodar o npm run dev novamente, para popular o tsconfig
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Head from 'next/head'
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({title = "Next js"}) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">{title}</a>
         </h1>
 
         <p className="description">
